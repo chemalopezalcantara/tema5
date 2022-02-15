372054f9f5f3eeb0140451d39774550c976ee221
Victor <Victor@WINDOWS11>
2022-02-15 19:39:42 +0100

    funciona todo, falta mover las excepciones a la clase excepciones

--- src/main/java/com/mycompany/navarro_sanchez_victor_prog05_tarea/ArticuloDeportivo.java
+++ src/main/java/com/mycompany/navarro_sanchez_victor_prog05_tarea/ArticuloDeportivo.java
@@ -11,10 +11,13 @@ package com.mycompany.navarro_sanchez_victor_prog05_tarea;
 public class ArticuloDeportivo {
     
     //atributos
-    String descripcion;
-    String codArticulo;
-    
-    //metodos para validad descripcion y codArticulo
+    private String descripcion;
+    private String codArticulo;
+    private String ciudad;
+    private String tienda;
+    private String codArticuloRed;
+    private String digitoControl;
+    private static int unidades;
     
     
     
@@ -37,28 +40,92 @@ public class ArticuloDeportivo {
     public void setCodArticulo(String codArticulo) {
         this.codArticulo = codArticulo;
     }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getTienda() {
+        return tienda;
+    }
+
+    public void setTienda(String tienda) {
+        this.tienda = tienda;
+    }
+
+    public String getCodArticuloRed() {
+        return codArticuloRed;
+    }
+
+    public void setCodArticuloRed(String codArticuloRed) {
+        this.codArticuloRed = codArticuloRed;
+    }
+
     
-    
-    
-    
-    
-    
-    
+
+    public String getDigitoControl() {
+        return digitoControl;
+    }
+
+    public void setDigitoControl(String digitoControl) {
+        this.digitoControl = digitoControl;
+    }
+
+    public static int getUnidades() {
+        return unidades;
+    }
+
+    public static void setUnidades(int unidades) {
+        ArticuloDeportivo.unidades = unidades;
+    }
+
     //3 contructores
         //Uno	con	valores	por	defecto.
         //Otro	recibiendo	todo	lo	necesario	para	dar	identidad	al	artículo.
         //Constructor	copia	a	partir	de	otro	objeto	artículo.	(como	se	ve	en	los	apuntes)
+    public ArticuloDeportivo(){
+        this.descripcion = "esta es la descripcion del articulo";
+        this.codArticulo = "130100009914";
+        this.ciudad = "13";
+        this.tienda = "01";
+        this.codArticuloRed = "000099";
+        this.digitoControl = "14";
+    }
 
-    public ArticuloDeportivo(String descripcion, String codArticulo) {
+    public ArticuloDeportivo(String descripcion, String codArticulo, String ciudad, String tienda, String codArticuloRed) {
         this.descripcion = descripcion;
         this.codArticulo = codArticulo;
+        this.ciudad = ciudad;
+        this.tienda = tienda;
+        this.codArticuloRed = codArticuloRed;
+        
+    }
+    
+    //constructor copia
+    public ArticuloDeportivo(ArticuloDeportivo objeto){
+        this.descripcion = objeto.getDescripcion();
+        this.codArticulo = objeto.getCodArticulo();
+        this.ciudad = objeto.getCiudad();
+        this.tienda = objeto.getTienda();
+        this.codArticuloRed = objeto.getCodArticuloRed();
+        this.digitoControl = objeto.getDigitoControl();
     }
 
+    
     //toString
+
     @Override
     public String toString() {
-        return "ArticuloDeportivo{" + "descripcion=" + descripcion + ", codArticulo=" + codArticulo + '}';
+        return "ArticuloDeportivo{" + "descripcion=" + descripcion + ", codArticulo=" + codArticulo + ", ciudad=" + ciudad + ", tienda=" + tienda + ", codArticuloRed=" + codArticuloRed + '}';
     }
+
+   
+    
+    
 
 
 
--- src/main/java/com/mycompany/navarro_sanchez_victor_prog05_tarea/LanzadorTienda.java
+++ src/main/java/com/mycompany/navarro_sanchez_victor_prog05_tarea/LanzadorTienda.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.navarro_sanchez_victor_prog05_tarea;
 import java.util.Scanner;
+
 /**
  *
  * @author Victor
@@ -20,29 +21,8 @@ public class LanzadorTienda {
       
       
       //pedir codigo de articulo
-      
-    System.out.println("introduce la descripcion");
-        String descripcion = leerTeclado();
+        obtenerDatosArticulo();
 
-    System.out.println("introduce el codigo de articulo: ");
-        String codArticulo = leerTeclado();
-
-        
-        if (validarDescripcion(descripcion)&&validarArticulo(codArticulo)){
-            System.out.println("la descripcion y el cod son correctos");
-            ArticuloDeportivo objeto = new ArticuloDeportivo(descripcion, codArticulo);
-            System.out.println(objeto.toString());
-        }else{
-            System.out.println("no se han introducido los datos correctamente");
-        }
-      
-      
-      
-        mostrarMenu();
-        
-        
-        
-        
         
     } //final del main
     
@@ -50,7 +30,7 @@ public class LanzadorTienda {
     
     //metodos
     
-    private static void mostrarMenu(){
+    private static void mostrarMenu(ArticuloDeportivo objeto){
         Scanner sc = new Scanner (System.in);
         try{
           int opcion = 0;  
@@ -75,29 +55,85 @@ public class LanzadorTienda {
                 
                 switch(opcion){
                     case 1:
-                        System.out.println("has escogido la opción 1");
-                        
-                        System.out.println("introduce el codigo de pedido: ");
-                        leerTeclado();
-                        
-                       
-                        
+                        System.out.println("has escodigo la opcion de mostrar articulo: ");
+                        System.out.println(objeto.getDescripcion());
+                        System.out.println("");
                         break;
                     case 2:
+                        System.out.println("has escodigo la opcion de mostrar la descripción del articulo");
+                        System.out.println(objeto.getDescripcion());
+                        System.out.println(" ");
                         break;
+                        
                     case 3:
+                        System.out.println("has escogido la opcion de mostrar el codigo de ciudad, es:");
+                        System.out.println(objeto.getCiudad());
                         break;
                     case 4:
+                        System.out.println("has escogido la opcion de mostrar el codigo de tienda, es:");
+                        System.out.println(objeto.getTienda());
                         break;
                     case 5:
+                        System.out.println("has escogido la opcion de mostrar el codigo reducido (6caracteres), es:");
+                        System.out.println(objeto.getCodArticuloRed());
                         break;
                     case 6:
+                        System.out.println("has escogido la opcion de mostrar el digito de control, es ");
+                        double doubleCiudad, doubleTienda, doubleCodArticuloRed;
+                        
+                        doubleCiudad =Double.parseDouble(objeto.getCiudad());
+                        doubleTienda = Double.parseDouble(objeto.getTienda());
+                        doubleCodArticuloRed = Double.parseDouble(objeto.getCodArticuloRed());
+                        System.out.println(obtDigControl(doubleCiudad, doubleTienda, doubleCodArticuloRed));
                         break;
+                        
                     case 7:
+                        System.out.println("has escogido la opción aumentar unidades, indica cuantas unidades deseas aumentar");
+                        int cantidadSumar = Integer.parseInt(leerTeclado());
+                        if(cantidadSumar>0){
+                           objeto.setUnidades(cantidadSumar); 
+                           System.out.println("SE HA AUMENTADO ");
+                            System.out.println(" ");
+                        }else{
+                            System.out.println("no puedes sumar numeros negativos");
+                        }
+                        
+                        
                         break;
                     case 8:
+                        System.out.println("has escogido la opción disminuir unidades");
+                        if(objeto.getUnidades()==0){
+                            System.out.println("NO HAY UNIDADES QUE RESTAR");
+                        }else{
+                            System.out.println("indica las unidades que deseas restar");
+                            int cantidadRestar = Integer.parseInt(leerTeclado());
+                            if(objeto.getUnidades()<cantidadRestar || cantidadRestar<0){
+                                System.out.println("no se puede realizar");
+                            }else{
+                                objeto.setUnidades(objeto.getUnidades()-cantidadRestar);
+                            }
+                            System.out.println("AHORA tienes estas unidades: " +objeto.getUnidades());
+                        }
                         break;
+                        
                     case 9:
+                        System.out.println("has escogido la opción comprobar unidades");
+                        objeto.getUnidades();
+                        if(objeto.getUnidades()==0){
+                            System.out.println("no hay unidades");
+                        }else{
+                            System.out.println("hay: " + objeto.getUnidades());
+                        }
+                        
+                        System.out.println(" ");
+                        break;
+                    case 10:
+                        System.out.println("la información del articulo es: ");
+                        System.out.println(objeto.toString());
+                        System.out.println(" ");
+                        System.out.println("************************************************************************************************************");
+                        System.out.println("**************************************hasta pronto de DECIMAS***********************************************");
+                        System.out.println("************************************************************************************************************");
                         break;
                 }
                 
@@ -116,24 +152,35 @@ public class LanzadorTienda {
     
     private static String leerTeclado(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("introduce el dato:");
+        try{
+            System.out.println("introduce el dato:");
         String cadena = sc.nextLine();
-        System.out.println( " ");
-        System.out.println( "has introducido:" + cadena);
-        
+        if(cadena.isEmpty()){
+            System.out.println("has introducido un valor en blanco, REPITE");
+            leerTeclado();
+        }else{
+            System.out.println( " ");
+            System.out.println( "has introducido:" + cadena);
+        }
         return cadena;
-        
-    }
+        }catch(Exception e){
+            return "se ha producido una excepcion al leer por teclado"+ e;
+        }
+
+    }//final de leer teclado
     
     private static int leerOpcion(){
         Scanner sc = new Scanner (System.in);
         System.out.println("introduce la opcion:");
         int opcion = sc.nextInt();
-        if(opcion >0 || 10>opcion){
+        
+        if(opcion >0 && 10>=opcion){
             System.out.println("la opcion es correcta");
             return opcion;
-        }else{
-            System.out.println("no has escogido un numero correcto");
+        }
+        else{
+            System.out.println("no has escogido un numero correcto, REPITE");
+            
             return 0;
         }
     } //final de leerOpcion
@@ -151,29 +198,65 @@ public class LanzadorTienda {
         }//final del false
     }//final del metodo validarDescripcion
 
-    private static boolean validarArticulo(String codArticulo){
-        int lonArticulo = codArticulo.length();
-        if((lonArticulo==12)&&validarNumArticulo(codArticulo)==true){
-            System.out.println("se ha creado el articulo correctamente");
+    
+    private static String extraerDatosCiudad(String codArticulo){
+    String returnRango = " ";
+    String rango = " ";
+    for(int i = 0; i<=1; i++){
+        rango = String.valueOf(codArticulo.charAt(i));
 
-            return true;
-        }else{
-            System.out.println("algo ha salido mal");
-            return false;
-        }//final del else
-    } //final validar articulo
+        returnRango +=rango;
+    }
+    return returnRango;
+}
+    private static String extraerDatosTienda(String codArticulo){
+        String returnRango = " ";
+        String rango = " ";
+        for(int i = 2; i<=3; i++){
+            rango = String.valueOf(codArticulo.charAt(i));
 
-    public static boolean validarNumArticulo(String codArticulo){
-        try {
-            Double.parseDouble(codArticulo);
-            return true;
-        }catch (NumberFormatException e){
-            System.out.println("no es un numero: "+ e );
-            return false;
+            returnRango +=rango;
         }
+        return returnRango;
+    }
+    private static String extraerDatosCodArticulo(String codArticulo){
+        String returnRango = " ";
+        String rango = " ";
+        for(int i = 4; i<=9; i++){
+            rango = String.valueOf(codArticulo.charAt(i));
+
+            returnRango +=rango;
+        }
+        return returnRango;
     }
     
-   
+    private static double obtDigControl(double ciudad, double tienda, double codArticuloRed){
+
+        double operacion = (ciudad+tienda+codArticuloRed)%99;
+        return operacion;
+    }
+    
+    private static void obtenerDatosArticulo(){
+         System.out.println("introduce la descripcion");
+    String descripcion = leerTeclado();
+    System.out.println("introduce el codigo de articulo: ");
+    String codArticulo = leerTeclado();
+    
+        if(validarDescripcion(descripcion)&& codArticulo.length()==12){
+            //instanciamos el objeto
+            
+            String ciudad = extraerDatosCiudad(codArticulo);
+            String tienda = extraerDatosTienda(codArticulo);
+            String codArticuloRed = extraerDatosCodArticulo(codArticulo);
+            
+            ArticuloDeportivo objeto = new ArticuloDeportivo(descripcion, codArticulo, ciudad, tienda, codArticuloRed);
+            System.out.println(objeto.toString());
+            mostrarMenu(objeto);
+            
+        }else{
+            System.out.println("no se cumplen con alguno de los requisitos");
+        } 
+      } //final de obtenerDatosArticulo
     
     
 } //final de la clase
